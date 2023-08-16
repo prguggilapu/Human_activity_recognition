b767ed30acae64eb45c0d398650b8db3d1d02566
prguggilap <ergpriyaankadevi@gmail.com>
2023-08-16 15:47:03 -0400

    Added ML models

---
+++ HAR.py
@@ -0,0 +1,413 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')  # Use this backend
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import features from features.txt file - 516 features
+features = list()
+with open('UCI HAR Dataset/features.txt') as f:
+    features = [line.split()[1] for line in f.readlines()]
+print('No of Features: {}'.format(len(features)))
+
+"""====================================================================================================================================================="""
+""" Obtain train data """
+
+X_train = pd.read_csv("UCI HAR Dataset/train/X_train.txt", delim_whitespace= True, header=None, encoding = 'latin-1')
+X_train.columns = features
+# add subject column to the dataframe
+X_train['subject'] = pd.read_csv("UCI HAR Dataset/train/subject_train.txt", header=None)
+
+y_train = pd.read_csv("UCI HAR Dataset/train/y_train.txt", names=['Activity'])
+y_train_labels = y_train.replace([1,2,3,4,5,6],['WALKING','WALKING_UPSTAIRS','WALKING_DOWNSTAIRS','SITTING','STANDING','LAYING'])
+
+
+# put all columns in a single dataframe
+train = X_train
+train['Activity'] = y_train
+train['ActivityName'] = y_train_labels
+train.sample()
+
+print(train.shape)
+
+"""============================================================================================================================================"""
+"""Obtain Test data"""
+X_test = pd.read_csv("UCI HAR Dataset/test/X_test.txt", delim_whitespace= True, header=None, encoding = 'latin-1')
+X_test.columns = features
+# add subject column to the dataframe
+X_test['subject'] = pd.read_csv("UCI HAR Dataset/test/subject_test.txt", header=None)
+y_test = pd.read_csv("UCI HAR Dataset/test/y_test.txt", names=['Activity'])
+y_test_labels = y_test.replace([1,2,3,4,5,6],['WALKING','WALKING_UPSTAIRS','WALKING_DOWNSTAIRS','SITTING','STANDING','LAYING'])
+# put all columns in a single dataframe
+test = X_test
+test['Activity'] = y_test
+test['ActivityName'] = y_test_labels
+test.sample()
+
+print(test.shape)
+
+"""=================================================================================================================================="""
+""" Data Cleaning"""
+## 1. Check for Duplicates
+
+print('No of duplicates in train: {}'.format(sum(train.duplicated())))
+print('No of duplicates in test : {}'.format(sum(test.duplicated())))
+
+"""## 2. Checking for NaN/null values"""
+
+print('We have {} NaN/Null values in train'.format(train.isnull().values.sum()))
+print('We have {} NaN/Null values in test'.format(test.isnull().values.sum()))
+
+"""## 3. Check for data imbalance"""
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.rcParams['font.family'] = 'Dejavu Sans'
+
+plt.figure(figsize=(16,8))
+plt.title('Data provided by each user', fontsize=20)
+sns.countplot(x='subject',hue='ActivityName', data = train)
+plt.show()
+
+plt.figure()
+plt.title('No of Datapoints per Activity', fontsize=15)
+sns.countplot(x = 'ActivityName', data = train)
+plt.xticks(rotation=90)
+plt.show()
+
+"""## 4. Changing feature names"""
+
+columns = train.columns
+
+# Removing '()' from column names
+columns = columns.str.replace('(','')
+columns = columns.str.replace(')','')
+columns = columns.str.replace('-', '')
+columns = columns.str.replace(',','')
+
+train.columns = columns
+test.columns = columns
+
+print(columns)
+
+"""## 5. Save this dataframe in a csv files"""
+
+train.to_csv('train.csv', index=False)
+test.to_csv('test.csv', index=False)
+
+"""====================================================================================================================================================="""
+"""EDA"""
+
+### 1. Stationary and Moving activities are completely different
+sns.set_palette("Set1", desat=0.80)
+facetgrid = sns.FacetGrid(train, hue='ActivityName',aspect=2)
+facetgrid.map(sns.histplot, 'tBodyAccMagmean', kde=False)
+facetgrid.map(sns.kdeplot, 'tBodyAccMagmean', color='red', label='KDE')
+
+# Add a legend
+facetgrid.add_legend()
+plt.annotate("Stationary Activities", xy=(-0.956,150), xytext=(-0.9, 250), size=20,\
+            va='center', ha='left',\
+            arrowprops=dict(arrowstyle="simple",connectionstyle="arc3,rad=0.1"))
+
+plt.annotate("Moving Activities", xy=(0,150), xytext=(0.2, 200), size=20,\
+            va='center', ha='left',\
+            arrowprops=dict(arrowstyle="simple",connectionstyle="arc3,rad=0.1"))
+plt.show()
+
+# for plotting purposes taking datapoints of each activity to a different dataframe
+df1 = train[train['Activity']==1]
+df2 = train[train['Activity']==2]
+df3 = train[train['Activity']==3]
+df4 = train[train['Activity']==4]
+df5 = train[train['Activity']==5]
+df6 = train[train['Activity']==6]
+
+fig, axes = plt.subplots(1, 2, figsize=(14, 7))
+
+# First subplot
+axes[0].set_title('Stationary Activities (Zoomed in)')
+sns.histplot(df4['tBodyAccMagmean'], color='r', kde=True, ax=axes[0], label='Sitting')
+sns.histplot(df5['tBodyAccMagmean'], color='m', kde=True, ax=axes[0], label='Standing')
+sns.histplot(df6['tBodyAccMagmean'], color='c', kde=True, ax=axes[0], label='Laying')
+axes[0].legend(loc='center')
+
+# Second subplot
+axes[1].set_title('Moving Activities')
+sns.histplot(df1['tBodyAccMagmean'], color='red', kde=True, ax=axes[1], label='Walking')
+sns.histplot(df2['tBodyAccMagmean'], color='blue', kde=True, ax=axes[1], label='Walking Up')
+sns.histplot(df3['tBodyAccMagmean'], color='green', kde=True, ax=axes[1], label='Walking Down')
+axes[1].legend(loc='center right')
+
+plt.tight_layout()
+plt.show()
+
+### 2. Magnitude of an acceleration can saperate it well"""
+
+plt.figure(figsize=(7,7))
+sns.boxplot(x='ActivityName', y='tBodyAccMagmean',data=train, showfliers=False, saturation=1)
+plt.ylabel('Acceleration Magnitude mean')
+plt.axhline(y=-0.7, xmin=0.1, xmax=0.9,dashes=(5,5), c='g')
+plt.axhline(y=-0.05, xmin=0.4, dashes=(5,5), c='m')
+plt.xticks(rotation=90)
+plt.show()
+
+"""__ Observations__:
+- If tAccMean is < -0.8 then the Activities are either Standing or Sitting or Laying.
+- If tAccMean is > -0.6 then the Activities are either Walking or WalkingDownstairs or WalkingUpstairs.
+- If tAccMean > 0.0 then the Activity is WalkingDownstairs.
+- We can classify 75% the Acitivity labels with some errors."""
+
+### 3. Position of GravityAccelerationComponants also matters
+plt.figure()
+sns.boxplot(x='ActivityName', y='angleXgravityMean', data=train)
+plt.axhline(y=0.08, xmin=0.1, xmax=0.9,c='m',dashes=(5,3))
+plt.title('Angle between X-axis and Gravity_mean', fontsize=15)
+plt.xticks(rotation = 40)
+plt.show()
+
+"""__ Observations__:
+* If angleX,gravityMean > 0 then Activity is Laying.
+* We can classify all datapoints belonging to Laying activity with just a single if else statement.
+"""
+
+"""====================================================================================================="""
+""" Apply t-SNE on the data"""
+
+# performs t-sne with different perplexity values and their repective plots..
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def perform_tsne(X_data, y_data, perplexities, n_iter=1000, img_name_prefix='t-sne'):
+
+    for index,perplexity in enumerate(perplexities):
+        # perform t-sne
+        print('\nperforming tsne with perplexity {} and with {} iterations at max'.format(perplexity, n_iter))
+        X_reduced = TSNE(verbose=2, perplexity=perplexity).fit_transform(X_data)
+        print('Done..')
+
+        # prepare the data for seaborn
+        print('Creating plot for this t-sne visualization..')
+        df = pd.DataFrame({'x':X_reduced[:,0], 'y':X_reduced[:,1] ,'label':y_data})
+
+        # draw the plot in appropriate place in the grid
+        sns.lmplot(data=df, x='x', y='y', hue='label', fit_reg=False, palette="Set1", markers=['^', 'v', 's', 'o', '1', '2'])
+        plt.title("perplexity : {} and max_iter : {}".format(perplexity, n_iter))
+        img_name = img_name_prefix + '_perp_{}_iter_{}.png'.format(perplexity, n_iter)
+        print('saving this plot as image in present working directory...')
+        plt.savefig(img_name)
+        plt.show()
+        print('Done')
+
+X_pre_tsne = train.drop(['subject', 'Activity','ActivityName'], axis=1)
+y_pre_tsne = train['ActivityName']
+perform_tsne(X_data = X_pre_tsne,y_data=y_pre_tsne, perplexities =[2,5,10,20,50])
+
+
+"""# Apply Machine Learning Models"""
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+print(train.shape, test.shape)
+
+x_train = train.drop(['subject', 'Activity', 'ActivityName'], axis=1)
+y_train = train.ActivityName
+
+# get x_test and y_test from test csv file
+x_test = test.drop(['subject', 'Activity', 'ActivityName'], axis=1)
+y_test = test.ActivityName
+
+print('x_train and y_train : ({},{})'.format(x_train.shape, y_train.shape))
+print('x_test  and y_test  : ({},{})'.format(x_test.shape, y_test.shape))
+
+labels=['LAYING', 'SITTING','STANDING','WALKING','WALKING_DOWNSTAIRS','WALKING_UPSTAIRS']
+
+"""# Function to plot the confusion matrix"""
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def perform_model(model, X_train, y_train, X_test, y_test, class_labels, cm_normalize=True, \
+                 print_cm=True, cm_cmap=plt.cm.Greens):
+    # to store results at various phases
+    results = dict()
+
+    # time at which model starts training
+    train_start_time = datetime.now()
+    print('training the model..')
+    model.fit(X_train, y_train)
+    print('Done \n \n')
+    train_end_time = datetime.now()
+    results['training_time'] =  train_end_time - train_start_time
+    print('training_time(HH:MM:SS.ms) - {}\n\n'.format(results['training_time']))
+
+
+    # predict test data
+    print('Predicting test data')
+    test_start_time = datetime.now()
+    y_pred = model.predict(X_test)
+    test_end_time = datetime.now()
+    print('Done \n \n')
+    results['testing_time'] = test_end_time - test_start_time
+    print('testing time(HH:MM:SS:ms) - {}\n\n'.format(results['testing_time']))
+    results['predicted'] = y_pred
+
+
+    # calculate overall accuracty of the model
+    accuracy = metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
+    # store accuracy in results
+    results['accuracy'] = accuracy
+    print('---------------------')
+    print('|      Accuracy      |')
+    print('---------------------')
+    print('\n    {}\n\n'.format(accuracy))
+
+
+    # confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    results['confusion_matrix'] = cm
+    if print_cm:
+        print('--------------------')
+        print('| Confusion Matrix |')
+        print('--------------------')
+        print('\n {}'.format(cm))
+
+    # plot confusin matrix
+    plt.figure(figsize=(8,8))
+    plt.grid()
+    plot_confusion_matrix(cm, classes=class_labels, normalize=True, title='Normalized confusion matrix', cmap = cm_cmap)
+    plt.show()
+
+    # get classification report
+    print('-------------------------')
+    print('| Classifiction Report |')
+    print('-------------------------')
+    classification_report = metrics.classification_report(y_test, y_pred)
+    # store report in results
+    results['classification_report'] = classification_report
+    print(classification_report)
+
+    # add the trained  model to the results
+    results['model'] = model
+
+    return results
+
+"""# Method to print the gridsearch Attribute"""
+
+def print_grid_search_attributes(model):
+    # Estimator that gave highest score among all the estimators formed in GridSearch
+    print('--------------------------')
+    print('|      Best Estimator     |')
+    print('--------------------------')
+    print('\n\t{}\n'.format(model.best_estimator_))
+
+
+    # parameters that gave best results while performing grid search
+    print('--------------------------')
+    print('|     Best parameters     |')
+    print('--------------------------')
+    print('\tParameters of best estimator : \n\n\t{}\n'.format(model.best_params_))
+
+
+    #  number of cross validation splits
+    print('---------------------------------')
+    print('|   No of CrossValidation sets   |')
+    print('--------------------------------')
+    print('\n\tTotal numbre of cross validation sets: {}\n'.format(model.n_splits_))
+
+
+    # Average cross validated score of the best estimator, from the Grid Search
+    print('--------------------------')
+    print('|        Best Score       |')
+    print('--------------------------')
+    print('\n\tAverage Cross Validate scores of best estimator : \n\n\t{}\n'.format(model.best_score_))
+
+import warnings
+warnings.filterwarnings("ignore")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+import itertools
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = 'DejaVu Sans'
+import seaborn as sns
+# To be able to save images on server
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot                                                              
+from sklearn.manifold import TSNE
+from sklearn.metrics import confusion_matrix
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+# Importing tensorflow
+np.random.seed(42)
+import tensorflow as tf
+
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import LSTM , BatchNormalization
+from keras.layers import Dense, Dropout
+from keras.regularizers import L1L2
+
+"""# Logistic Regression with Grid Search"""
+
+parameters = {'C':[0.01, 0.1, 1, 10, 20, 30], 'penalty':['l2','l1']}
+log_reg = linear_model.LogisticRegression(max_iter=100)
+log_reg_grid = GridSearchCV(log_reg, param_grid=parameters, cv=3, verbose=1, n_jobs=-1)
+log_reg_grid_results =  perform_model(log_reg_grid, x_train, y_train, x_test, y_test, class_labels=labels)
+
+matplotlib.use('TkAgg')
+plt.figure()
+plt.grid()
+plot_confusion_matrix(log_reg_grid_results['confusion_matrix'], classes=labels, cmap=plt.cm.Greens)
+plt.show()
+
+print_grid_search_attributes(log_reg_grid_results['model'])
+
+"""# Linear SVC with GridSearch"""
+parameters = {'C':[0.125, 0.5, 1, 2, 8, 16]}
+lr_svc = LinearSVC(tol=0.00005)
+lr_svc_grid = GridSearchCV(lr_svc, param_grid=parameters, n_jobs=-1, verbose=1)
+lr_svc_grid_results = perform_model(lr_svc_grid, x_train, y_train, x_test, y_test, class_labels=labels)
+
+plt.figure(figsize=(8,8))
+plt.grid()
+plot_confusion_matrix(lr_svc_grid_results['confusion_matrix'], classes=labels, cmap=plt.cm.Greens)
+plt.show()
+
+print_grid_search_attributes(lr_svc_grid_results['model'])
